084af8ab6038b8363da5bd57ab07dd5bac70d2e1
Nathan Reuter <nathan0reuter@gmail.com>
2015-02-11 12:14:53 -0200

    Packges changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,14 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-
-        configs: configs,
-
-        mozjpeg: mozjpeg,
-
-
         // Automatically inject Bower components
         wiredep: {
-                src: ['index.html']
+            task: {
+                src: [
+                './index.html'
+                ]
+
+            }
         }
+    });
 };
