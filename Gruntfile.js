1748b8568279474ac8c385a4d3a64dde16720149
Nathan Reuter <nathan0reuter@gmail.com>
2016-05-16 12:52:27 -0300

    file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,5 @@
 'use strict';
 
 module.exports = function (grunt) {
-
-    require('load-grunt-tasks')(grunt);
-
-    grunt.initConfig({
-        // Automatically inject Bower components
-        wiredep: {
-            task: {
-                src: [
-                'index.html'
-                ]
-
-            }
-        }
-    });
+    
 };
